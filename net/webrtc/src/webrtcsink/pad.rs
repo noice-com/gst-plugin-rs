6c0fd6f9da5c71bcc54fef9247ae18b6ce758c1a
Antti Tapaninen <aet@aet.fi>
2024-04-21 15:48:12 -0700

    webrtcsink: add initial simulcast support

--- net/webrtc/src/webrtcsink/pad.rs
+++ net/webrtc/src/webrtcsink/pad.rs
@@ -12,6 +12,9 @@ pub struct WebRTCSinkPad {
 #[derive(Debug, Default)]
 struct Settings {
     msid: Option<String>,
+    mid: Option<String>,
+    rid: Option<String>,
+    rtp_extra: Option<gst::Structure>,
 }
 
 #[glib::object_subclass]
@@ -24,10 +27,25 @@ impl ObjectSubclass for WebRTCSinkPad {
 impl ObjectImpl for WebRTCSinkPad {
     fn properties() -> &'static [glib::ParamSpec] {
         static PROPS: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
-            vec![glib::ParamSpecString::builder("msid")
-                .flags(glib::ParamFlags::READWRITE | gst::PARAM_FLAG_MUTABLE_READY)
-                .blurb("Remote MediaStream ID in use for this pad")
-                .build()]
+            vec![
+                glib::ParamSpecString::builder("msid")
+                    .flags(glib::ParamFlags::READWRITE | gst::PARAM_FLAG_MUTABLE_READY)
+                    .blurb("Remote MediaStream ID in use for this pad")
+                    .build(),
+                glib::ParamSpecString::builder("mid")
+                    .flags(glib::ParamFlags::READWRITE | gst::PARAM_FLAG_MUTABLE_READY)
+                    .blurb("MID attribute for this pad")
+                    .build(),
+                glib::ParamSpecString::builder("rid")
+                    .flags(glib::ParamFlags::READWRITE | gst::PARAM_FLAG_MUTABLE_READY)
+                    .blurb("RID attribute for this pad")
+                    .build(),
+                glib::ParamSpecBoxed::builder::<gst::Structure>("rtp-extra")
+                    .blurb("RTP payload extras")
+                    .readwrite()
+                    .mutable_ready()
+                    .build(),
+            ]
         });
         PROPS.as_ref()
     }
@@ -39,6 +57,21 @@ impl ObjectImpl for WebRTCSinkPad {
                     .get::<Option<String>>()
                     .expect("type checked upstream")
             }
+            "mid" => {
+                settings.mid = value
+                    .get::<Option<String>>()
+                    .expect("type checked upstream")
+            }
+            "rid" => {
+                settings.rid = value
+                    .get::<Option<String>>()
+                    .expect("type checked upstream")
+            }
+            "rtp-extra" => {
+                settings.rtp_extra = value
+                    .get::<Option<gst::Structure>>()
+                    .expect("type checked upstream");
+            }
             name => panic!("no writable property {name:?}"),
         }
     }
@@ -46,6 +79,9 @@ impl ObjectImpl for WebRTCSinkPad {
         let settings = self.settings.lock().unwrap();
         match pspec.name() {
             "msid" => settings.msid.to_value(),
+            "mid" => settings.mid.to_value(),
+            "rid" => settings.rid.to_value(),
+            "rtp-extra" => settings.rtp_extra.to_value(),
             name => panic!("no readable property {name:?}"),
         }
     }
